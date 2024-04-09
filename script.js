ab50186d723bcdd9eee9d06361ee1e9651128e51
PROTIK SARKAR <sarkarprotik240@gmail.com>
2024-04-09 13:42:55 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,13 @@
+function micFunction() {
+    alert("Hello");
+    navigator.mediaDevices
+        .getUserMedia({ video: false, audio: true })
+        .then((stream) => {
+            window.localStream = stream;
+            window.localAudio.srcObject = stream;
+            window.localAudio.autoplay = true;
+        })
+        .catch((err) => {
+            console.error(`you got an error: ${err}`);
+        });
+}
