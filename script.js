a4c380ccd0f8f20b35517cb9f51155ae8579d52d
PROTIK SARKAR <sarkarprotik240@gmail.com>
2024-04-09 18:40:21 +0530

    added new data

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-function micFunction() {
-    alert("Hello");
-    navigator.mediaDevices
-        .getUserMedia({ video: false, audio: true })
-        .then((stream) => {
-            window.localStream = stream;
-            window.localAudio.srcObject = stream;
-            window.localAudio.autoplay = true;
+const mic_btn = document.querySelector("#mic");
+const text_area = document.querySelector(".text");
+
+
+// const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList;
+// const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+
+// const speechRecognitionList = new SpeechGrammarList();
+
+// recognition.grammars = speechRecognitionList;
+// recognition.lang = "en-US";
+// recognition.maxAlternatives = 1;
+
+mic_btn.addEventListener('click', function () {
+    try {
+        var speech = true;
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        const recognition = new SpeechRecognition();
+
+        recognition.interimResults = true;
+        recognition.continuous = false;
+
+
+        recognition.addEventListener('result', e => {
+            const transcript = Array.from(e.results)
+                .map(result = result[0])
+                .map(result = result.transcript)
+
+            text_area.innerHTML = transcript;
         })
-        .catch((err) => {
-            console.error(`you got an error: ${err}`);
-        });
-}
+
+        if (speech == true) {
+            recognition.start();
+        }
+    } catch (error) {
+        console.log("Errors: " + error)
+    }
+});
